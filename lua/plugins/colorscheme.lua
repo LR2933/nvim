c1a8baa4b53bac05587e17236b18b2e9b0e9ad88
lr <r2933985616@gmail.com>
2026-01-04 19:41:21 +0800

    修改配色方案为catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,36 +1,29 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000, -- 确保它在其他插件之前加载
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000, -- 确保主题在其他插件之前加载
     config = function()
-      -- 配置 Gruvbox（可选）
-      require("gruvbox").setup({
-        terminal_colors = true, -- 开启终端颜色映射
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          emphasis = true,
-          comments = true,
-          operators = false,
-          folds = true,
+      require("catppuccin").setup({
+        flavour = "mocha", -- 选填: latte, frappe, macchiato, mocha (默认)
+        transparent_background = false, -- 如果想要透明背景可以改为 true
+        term_colors = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = true,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+          -- 根据你使用的插件开启更多集成
         },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- 反转一些颜色
-        contrast = "hard", -- 强烈推荐使用 "hard" 对比度，看起来最精神
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
       })
-      
-      -- 应用主题
-      vim.cmd("colorscheme gruvbox")
+
+      -- 设置为当前主题
+      vim.cmd.colorscheme("catppuccin")
     end,
-  }
+  },
 }
